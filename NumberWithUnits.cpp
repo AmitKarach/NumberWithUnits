c46e0e757496361e01468ccfe4885504802a77d1
AmitKarach <karach.amit30@gmail.com>
2021-04-28 12:40:08 +0300

    r

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -12,186 +12,212 @@
 
 const double EPS = 0.0001;
 using namespace std;
-namespace ariel{
-            std::map<string,std::map<string,double>> units;
+namespace ariel
+{
+    std::map<string, std::map<string, double>> units;
 
-             void  NumberWithUnits::read_units(ifstream& file)
-             {
-                string buff, unitA, unitB;
-                double size1=0, size2=0;
-                while(file >> size1 >> unitA >> buff >> size2 >> unitB)
-                {                
-                    units[unitA][unitB] = size2;
-                    units[unitB][unitA] = 1/size2;
-                    update_map(unitA, unitB);
-                    update_map(unitB, unitA);                  
-                }
-             }
-
-            void NumberWithUnits::update_map(const string &src, const string &dest){
-                for(auto &unit : units[dest]){
-                    if(unit.first != src){
-                        double new_size = units[src][dest]*unit.second;
-                        units[src][unit.first] = new_size;
-                        units[unit.first][src] = 1/new_size;
-                    }
-                }
-            }
-
-             NumberWithUnits::NumberWithUnits(double s,const string& u)
-            {
-                if(units.find(u) == units.end()){
-                    throw invalid_argument(u + " invalid unit");
-                 }   
-                this->size=s;
-                this->unit=u;
-            }
-
-        double NumberWithUnits::adjust_unit(const NumberWithUnits &unit1) const{
-            if(!(unit == unit1.unit || units[unit].find(unit1.unit) != units[unit].end())){
-                throw invalid_argument("diffrent family");
-            }
-            if(unit == unit1.unit)
-            {
-                return unit1.size;
-            }
-            return unit1.size*units[unit1.unit][unit];
+    void NumberWithUnits::read_units(ifstream &file)
+    {
+        string buff, unitA, unitB;
+        double size1 = 0, size2 = 0;
+        while (file >> size1 >> unitA >> buff >> size2 >> unitB)
+        {
+            units[unitA][unitB] = size2;
+            units[unitB][unitA] = 1 / size2;
+            update_map(unitA, unitB);
+            update_map(unitB, unitA);
+        }
     }
 
-            //operator: +,+=,-,-=
-            NumberWithUnits operator+(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                 return NumberWithUnits(number1.size + number1.adjust_unit(number2),number1.unit);
-            }
-    	    
-            NumberWithUnits operator+(const NumberWithUnits& number, double num){
-                 return NumberWithUnits(number.size+num, number.unit);
-            }
-            const NumberWithUnits& operator+(const NumberWithUnits &number){
-                 return number;
-            }
-
-            NumberWithUnits operator-(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                 return NumberWithUnits(number1.size - number1.adjust_unit(number2),number1.unit);
-             }
-             
-            
-             NumberWithUnits operator-(const NumberWithUnits& number, double num){
-                 return NumberWithUnits(number.size-num, number.unit);
-             }
-
-            NumberWithUnits operator-(const NumberWithUnits& number){
-               return NumberWithUnits(-number.size, number.unit);
-            } 
-
-            NumberWithUnits& NumberWithUnits::operator+=(const NumberWithUnits& number){
-                this->size+=this->adjust_unit(number);
-                return *this;
-            }
-            
-            NumberWithUnits& NumberWithUnits::operator-=(const NumberWithUnits& number){
-                this->size-=this->adjust_unit(number);
-                return *this;
-            }
-           
-            
-             
-            
-            //operator: ++,--,*
-            NumberWithUnits& operator++( NumberWithUnits& number){
-               number.size++;
-               return number;
-            }     
-            NumberWithUnits& operator--( NumberWithUnits& number){
-                number.size--;
-                return number; 
-            }    
-            NumberWithUnits operator++( NumberWithUnits& number, int num){
-                NumberWithUnits befor(number.size, number.unit);
-                number.size++;
-                return befor;
-            }         
-            NumberWithUnits operator--( NumberWithUnits& number, int num)
+    void NumberWithUnits::update_map(const string &src, const string &dest)
+    {
+        for (auto &unit : units[dest])
+        {
+            if (unit.first != src)
             {
-                NumberWithUnits befor(number.size, number.unit);
-                number.size--;
-                return befor;
-            }  
-            NumberWithUnits operator*( const NumberWithUnits &number,const double &num){
-                return NumberWithUnits(number.size*num, number.unit);
-             }
-           
-             NumberWithUnits operator*(const double &num,const NumberWithUnits &number){
-                return NumberWithUnits(number.size*num, number.unit);
-             } 
-
-            //compare , >,>=,<,<=,==,!=
-            int compare(const NumberWithUnits &number1, const NumberWithUnits &number2) {
-        
-                NumberWithUnits n2_con(number1.adjust_unit(number2), number1.unit);
-                double check= (number1.size - n2_con.size);
-                double check_positive=fabs(check);
-                int ans=0;
-                if (check_positive<EPS){
-                    ans=0;
-                }
-                else if(check>0) {
-                        ans= 1;
-                    }
-                else{
-                    ans=-1;
-                }
-        
-                return ans;
+                double new_size = units[src][dest] * unit.second;
+                units[src][unit.first] = new_size;
+                units[unit.first][src] = 1 / new_size;
             }
+        }
+    }
 
-            bool operator==(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                return (fabs(number1.size - number1.adjust_unit(number2)) <= EPS);
-             }
+    NumberWithUnits::NumberWithUnits(double s, const string &u)
+    {
+        if (units.find(u) == units.end())
+        {
+            throw invalid_argument(u + " invalid unit");
+        }
+        this->size = s;
+        this->unit = u;
+    }
 
-            bool operator!=(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                return number1.size != number1.adjust_unit(number2);
-             }
-            bool operator>=(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                return compare(number1, number2) >= 0;
-             }
-             bool operator<(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                return compare(number1, number2) < 0;
-             }
-             bool operator>(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                return compare(number1, number2) > 0;
-             }
-               bool operator<=(const NumberWithUnits& number1, const NumberWithUnits& number2){
-                return compare(number1, number2) <= 0;
-             }
-            //operator:input,output
-             std::ostream& operator<< (std::ostream& output, const NumberWithUnits& number){
-                output << number.size << "[" << number.unit << ']';
-                return output;
-            }
+    double NumberWithUnits::adjust_unit(const NumberWithUnits &unit1) const
+    {
+        if (!(unit == unit1.unit || units[unit].find(unit1.unit) != units[unit].end()))
+        {
+            throw invalid_argument("diffrent family");
+        }
+        if (unit == unit1.unit)
+        {
+            return unit1.size;
+        }
+        return unit1.size * units[unit1.unit][unit];
+    }
 
-            istream &operator>>(istream &input, NumberWithUnits &n)
-            {
-                char cha = 0;
-                string unit;
-                double size = 0;
-                input >> size >> cha >> unit >> cha;
-                
-                if (unit[unit.length() - 1] == ']')
-                {
-                    unit.pop_back();
-                }
-                if (cha == '-')
-                {
-                    input.putback('-');
-                }
-                if(units.find(unit) == units.end())
-                {
-                    throw std::invalid_argument(unit + " is not a valid unit!");
-                }
-                n.size =size;
-                n.unit=unit;
-                return input;
-            }
+    //operator: +,+=,-,-=
+    NumberWithUnits operator+(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return NumberWithUnits(number1.size + number1.adjust_unit(number2), number1.unit);
+    }
 
+    NumberWithUnits operator+(const NumberWithUnits &number, double num)
+    {
+        return NumberWithUnits(number.size + num, number.unit);
+    }
+    const NumberWithUnits &operator+(const NumberWithUnits &number)
+    {
+        return number;
+    }
+
+    NumberWithUnits operator-(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return NumberWithUnits(number1.size - number1.adjust_unit(number2), number1.unit);
+    }
+
+    NumberWithUnits operator-(const NumberWithUnits &number, double num)
+    {
+        return NumberWithUnits(number.size - num, number.unit);
+    }
+
+    NumberWithUnits operator-(const NumberWithUnits &number)
+    {
+        return NumberWithUnits(-number.size, number.unit);
+    }
+
+    NumberWithUnits &NumberWithUnits::operator+=(const NumberWithUnits &number)
+    {
+        this->size += this->adjust_unit(number);
+        return *this;
+    }
+
+    NumberWithUnits &NumberWithUnits::operator-=(const NumberWithUnits &number)
+    {
+        this->size -= this->adjust_unit(number);
+        return *this;
+    }
+
+    //operator: ++,--,*
+    NumberWithUnits &operator++(NumberWithUnits &number)
+    {
+        number.size++;
+        return number;
+    }
+    NumberWithUnits &operator--(NumberWithUnits &number)
+    {
+        number.size--;
+        return number;
+    }
+    NumberWithUnits operator++(NumberWithUnits &number, int num)
+    {
+        NumberWithUnits befor(number.size, number.unit);
+        number.size++;
+        return befor;
+    }
+    NumberWithUnits operator--(NumberWithUnits &number, int num)
+    {
+        NumberWithUnits befor(number.size, number.unit);
+        number.size--;
+        return befor;
+    }
+    NumberWithUnits operator*(const NumberWithUnits &number, const double &num)
+    {
+        return NumberWithUnits(number.size * num, number.unit);
+    }
+
+    NumberWithUnits operator*(const double &num, const NumberWithUnits &number)
+    {
+        return NumberWithUnits(number.size * num, number.unit);
+    }
+
+    //compare , >,>=,<,<=,==,!=
+    int compare(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+
+        NumberWithUnits n2_con(number1.adjust_unit(number2), number1.unit);
+        double check = (number1.size - n2_con.size);
+        double check_positive = fabs(check);
+        int ans = 0;
+        if (check_positive < EPS)
+        {
+            ans = 0;
+        }
+        else if (check > 0)
+        {
+            ans = 1;
+        }
+        else
+        {
+            ans = -1;
+        }
+
+        return ans;
+    }
+
+    bool operator==(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return (fabs(number1.size - number1.adjust_unit(number2)) <= EPS);
+    }
+
+    bool operator!=(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return number1.size != number1.adjust_unit(number2);
+    }
+    bool operator>=(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return compare(number1, number2) >= 0;
+    }
+    bool operator<(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return compare(number1, number2) < 0;
+    }
+    bool operator>(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return compare(number1, number2) > 0;
+    }
+    bool operator<=(const NumberWithUnits &number1, const NumberWithUnits &number2)
+    {
+        return compare(number1, number2) <= 0;
+    }
+    //operator:input,output
+    std::ostream &operator<<(std::ostream &output, const NumberWithUnits &number)
+    {
+        output << number.size << "[" << number.unit << ']';
+        return output;
+    }
+
+    istream &operator>>(istream &input, NumberWithUnits &n)
+    {
+        char cha = 0;
+        string unit;
+        double size = 0;
+        input >> size >> cha >> unit >> cha;
+
+        if (unit[unit.length() - 1] == ']')
+        {
+            unit.pop_back();
+        }
+        if (cha == '-')
+        {
+            input.putback('-');
+        }
+        if (units.find(unit) == units.end())
+        {
+            throw std::invalid_argument(unit + " is not a valid unit!");
+        }
+        n.size = size;
+        n.unit = unit;
+        return input;
+    }
 
 }
